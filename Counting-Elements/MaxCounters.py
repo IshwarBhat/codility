d99bbfb10b45d3b73ad4588848c9eeba9c64769b
IshwarBhat <ishwrbhat@gmail.com>
2015-09-30 15:52:33 +0530

    Update MaxCounters.py

--- Counting-Elements/MaxCounters.py
+++ Counting-Elements/MaxCounters.py
@@ -1,13 +1,20 @@
-# https://codility.com/demo/results/trainingTC7JSX-8E9/
+# https://codility.com/demo/results/training4DKVMD-3KM/
 def solution(N, A):
     counters = N * [0]
     max_counters = 0
+    last_update = 0
     for elem in A:
         if elem == N+1:
-            counters = N * [max_counters]
+            last_update = max_counters
         else:
-            this_elem = counters[elem-1] + 1
-            counters[elem-1] = this_elem
-            if this_elem > max_counters:
-                max_counters = this_elem
+            if counters[elem-1] < last_update:
+                counters[elem-1] = last_update
+            counters[elem-1] += 1
+            if counters[elem-1] > max_counters:
+                max_counters = counters[elem-1]
+    for i in xrange(N):
+        if counters[i] < last_update:
+            counters[i] = last_update
     return counters
+
+
